8f78bd06de04af6c52e6023d052887d211d3f611
jessiegibson <jessie@thirstynyc.com>
2015-09-09 13:40:58 -0400

    initally adding this half built wordpress theme

---
+++ contact.php
@@ -0,0 +1,49 @@
+<?php 
+// Start of the Contact Page
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="favicon.ico">
+
+    <title>Request Garage Door Estimate</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="sign-up.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
+  </head>
+
+  <body>
+
+    <div class="container">
+      <div class="row" id="form-wrapper">
+        <form class="form-signin">
+          <h2 class="form-signin-heading">Request Estimate</h2>
+          <label for="inputFName" class="sr-only">First Name</label>
+          <input type="text" id="inputFName" class="form-control" placeholder="Enter First Name" required autofocus>
+          <label for="inputEmail" class="sr-only">Email address</label>
+          <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
+          <label for="inputPhone" class="sr-only">Contact Number</label>
+          <input type="tel" id="inputPhone" class="form-control" placeholder="Contact Phone Number" required>
+          <button class="btn btn-lg btn-primary btn-block" type="submit">Request Contact</button>
+        </form>
+      </div>
+    </div> <!-- /container -->
+
+
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
+  </body>
+</html>
+<?php 
+
+
